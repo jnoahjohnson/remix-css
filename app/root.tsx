ecc3c7cddbfa0c943e3380e15fe32e980c4aea56
Noah Johnson <jnoahjohnson@gmail.com>
2022-05-21 13:39:01 -0600

    Add index.sx stuff

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,6 +7,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "~/styles/root.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: true },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap",
+  },
+];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,132 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import React from "react";
+
+import styles from "~/styles/index.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
+type RemixStack = {
+  imgSrc: string;
+  title: string;
+  background: string;
+};
+
+export const loader: LoaderFunction = () => {
+  const stacks: RemixStack[] = [
+    {
+      imgSrc:
+        "https://images.unsplash.com/photo-1611191796022-2be279cc9957?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988",
+      title: "Blues",
+      background: "blue",
+    },
+    {
+      imgSrc:
+        "https://images.unsplash.com/photo-1575672913784-11a7cd4f25f4?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769",
+      title: "Indie",
+      background: "yellow",
+    },
+    {
+      imgSrc:
+        "https://images.unsplash.com/photo-1576109188032-aed32779c007?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986",
+      title: "Grunge",
+      background: "red",
+    },
+    {
+      imgSrc:
+        "https://images.unsplash.com/photo-1471995606044-d5222f0f91d2?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770",
+      title: "Reggae",
+      background: "green",
+    },
+    {
+      imgSrc:
+        "https://images.unsplash.com/photo-1559424452-eeb3a13ffe2b?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770",
+      title: "Disco",
+      background: "purple",
+    },
+    {
+      imgSrc:
+        "https://images.unsplash.com/photo-1647953960058-38dbef424e1b?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1765",
+      title: "Polka",
+      background: "pink",
+    },
+  ];
+
+  return { stacks };
+};
+
 export default function Index() {
+  const { stacks } = useLoaderData<{ stacks: RemixStack[] }>();
+
+  const COLORS: any = {
+    blue: "#1e3a8a",
+    red: "#7f1d1d",
+    green: "#14532d",
+    yellow: "#713f12",
+    orange: "#7c2d12",
+    purple: "#581c87",
+    pink: "#831843",
+  };
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main>
+      <div className="content-wrapper">
+        <header className="main-header">
+          <img
+            src="/images/RemixStacks.png"
+            alt="Remix Stacks"
+            className="logo"
+          />
+          <nav>
+            <ul className="nav-links">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/" className="active">
+                  Explore
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/"
+                  className="nav-button"
+                  style={
+                    {
+                      "--background-color ": "#F42435",
+                    } as React.CSSProperties
+                  }
+                >
+                  Submit
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <section className="grid-section">
+          {stacks.map((stack) => (
+            <article key={stack.title} className="grid-item">
+              <img
+                src={stack.imgSrc}
+                alt={stack.title}
+                className="grid-image"
+              />
+              <div
+                className="image-cover"
+                style={
+                  {
+                    "--background-color": COLORS[stack.background],
+                  } as React.CSSProperties
+                }
+              />
+              <p className="grid-item-text">{stack.title}</p>
+            </article>
+          ))}
+        </section>
+      </div>
+    </main>
   );
 }
